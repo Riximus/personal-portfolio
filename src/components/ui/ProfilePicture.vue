3b7388f1cd5c3a42c05a75e62b0e1c3c092456b1
Riximus <riximuslol@gmail.com>
2022-11-01 14:57:50 +0100

    🍱📱 updated responsive profile picture

--- src/components/ui/ProfilePicture.vue
+++ src/components/ui/ProfilePicture.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="profile-frame">
 
-    <img id="profile-frame-image" src="../../assets/images/pictures/profilepic_1188.webp" fetchpriority="high"
+    <img id="profile-frame-image" src="../../assets/images/pictures/profilepic_1000.jpg" fetchpriority="high"
          srcset="
-         ../../assets/images/pictures/profilepic_1188.webp 1188w,
-         ../../assets/images/pictures/profilepic_891.webp 891w,
-         ../../assets/images/pictures/profilepic_594.webp 594w,
-         ../../assets/images/pictures/profilepic_297.webp 297w"
-         sizes="35vw"
+         ../../assets/images/pictures/profilepic_1000.jpg 1000w,
+         ../../assets/images/pictures/profilepic_800.jpg 800w,
+         ../../assets/images/pictures/profilepic_700.jpg 700w,
+         ../../assets/images/pictures/profilepic_600.jpg 600w,
+         ../../assets/images/pictures/profilepic_500.jpg 500w,
+         ../../assets/images/pictures/profilepic_400.jpg 400w,
+         ../../assets/images/pictures/profilepic_350.jpg 350w,
+         ../../assets/images/pictures/profilepic_300.jpg 300w,
+         ../../assets/images/pictures/profilepic_250.jpg 250w,
+         ../../assets/images/pictures/profilepic_200.jpg 200w"
+         sizes="30vw"
          alt="Profile picture of me"/>
-
   </div>
 </template>
 
